420de59fc04f99f907360db6d3d3935f48a81378
Minh Le <baminh.letran@gmail.com>
2024-02-23 22:43:27 +0700

    Fix the loading state

--- src/pages/home/components/FoodLoader.tsx
+++ src/pages/home/components/FoodLoader.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 import ContentLoader from 'react-content-loader';
 
 const FoodLoader = () => (
-  <ContentLoader width={'100%'} backgroundColor="#fff" foregroundColor="#dedede" speed={2} height={300} viewBox="0 0 400 460">
-    <rect x="0" y="0" rx="8" ry="8" width="400" height="400" />
+  <ContentLoader
+    width={'100%'}
+    backgroundColor="#fff"
+    foregroundColor="#dedede"
+    speed={2}
+    height={300}
+    viewBox="0 0 400 460"
+    preserveAspectRatio="none"
+  >
+    <rect x="0" y="0" rx="8" ry="8" width="100%" height="400" />
     <rect x="0" y="416" rx="8" ry="8" width="176" height="14" />
     <rect x="0" y="442" rx="8" ry="8" width="80" height="14" />
     <rect x="90" y="442" rx="8" ry="8" width="80" height="14" />
